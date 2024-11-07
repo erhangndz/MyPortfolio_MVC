a1942a56f9080d459bc8bf155e5642760854addc
Erhan Gündüz <erhangndz@live.com>
2024-11-07 22:15:04 +0300

    5. ders sonu

---
+++ MyPortfolio_MVC/Controllers/EducationController.cs
@@ -0,0 +1,28 @@
+﻿using MyPortfolio_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio_MVC.Controllers
+{
+    public class EducationController : Controller
+    {
+        MyPortfolioDb6Entities db = new MyPortfolioDb6Entities();
+        public ActionResult Index()
+        {
+            var educations = db.TblEducations.ToList();
+            return View(educations);
+        }
+
+        public ActionResult DeleteEducation(int id)
+        {
+
+            var value = db.TblEducations.Find(id);
+            db.TblEducations.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
